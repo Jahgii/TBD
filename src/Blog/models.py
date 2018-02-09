cdb6b91a5c0cae4a8cfd3f791b75fb7c03b86b21
Jahgii <jahg.huerta@gmail.com>
2018-02-09 09:05:06 -0800

    slug

--- src/Blog/models.py
+++ src/Blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.utils.encoding import smart_text
 from django.utils import timezone
-
+from django.utils.text import slugify
+from django.db.models.signals import post_save
 from .validators import validate_algo, validate_author_email
 # Create your models here.
 
@@ -17,10 +18,26 @@ class Post(models.Model):
     title = models.CharField(max_length=250, default='NewPost',verbose_name='post title')
     content = models.TextField(null=True, blank=True)
     publish = models.CharField(max_length=120, choices=PUBLISH_CHOICES, default="draft")
+    slug = models.SlugField(null=True, blank=True)
     view_count = models.IntegerField(default=0)
     publish_date = models.DateField(auto_now=False, auto_now_add=False, default=timezone.now)
     author_email = models.EmailField(max_length = 240, validators=[validate_author_email, validate_algo], null=True, blank=True)
 
+
     def __str__(self):
         return smart_text(self.content)
-    pass
+
+    def save (self, *args, **kwargs):
+        print ("1ra Fase")
+        if not self.slug:
+            if self.title:
+                self.slug = slugify (self.title)
+        super (Post, self).save(*args, **kwargs)
+
+def post_model_post_save_receiver(sender, instance, created, *args, **kwargs):
+    print ("2da Fase")
+    if not instance.slug and instance.title:
+        instance.slug = slugify(instance.title)
+        instance.save()
+
+post_save.connect(post_model_post_save_receiver, sender = Post)
